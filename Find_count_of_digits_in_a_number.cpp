c45e3cb5c213d9a7b998f206bb00f655d1b7fa12
Kapilshrma99 <kapilsharma09311@gmail.com>
2024-07-15 19:53:49 +0530

    Find count of digits in a number

---
+++ Find_count_of_digits_in_a_number.cpp
@@ -0,0 +1,16 @@
+// Find count of digits in a number
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"enter";
+    int digit;
+    cin>>digit;
+    int count=0;
+    while(digit!=0){}
+        count++;
+        digit= digit / 10;
+    }
+    cout<<count;
+    return 0;
+}
